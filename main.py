e09ca1195c70da9adf49e2773d227fbbd7ef5845
yash-sojitra-20 <yash.sojitra.2020@gmail.com>
2024-02-24 17:52:01 +0530

    sample_query solved

--- main.py
+++ main.py
@@ -19,3 +19,20 @@ mydatabase = "tshirts"
 pg_uri = f"postgresql+psycopg2://{username}:{password}@{host}:{port}/{mydatabase}"
 db = SQLDatabase.from_uri(pg_uri)
 print(db.table_info)
+
+
+#sample_query solution using chain
+from langchain.chains import create_sql_query_chain
+db_chain = create_sql_query_chain(llm, db)
+
+# correct -1
+print("qns 1")
+qns1 = db_chain.invoke({"question": "How many t-shirts do we have left for nike in extra small size and white color?"})
+print(qns1)
+ans1=db.run(qns1)
+print(ans1)
+
+# correct-2
+qns2 = "SELECT SUM(price*stock_quantity) FROM t_shirts WHERE size = 'S'"
+ans2=db.run(qns2)
+print(ans2)
