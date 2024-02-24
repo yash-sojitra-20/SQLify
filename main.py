4ad5b695e0fe0ebb48a8d2aaa70bad222c4a683b
ompatel22 <omupatel22@gmail.com>
2024-02-24 16:45:38 +0530

    palm llm addedpython -u

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print("hello")
-print("hi")
-print("test")
+from langchain.llms import GooglePalm
+llm= GooglePalm(google_api_key="AIzaSyATg2P9z5r2nOQg29F0ChNBTIWmmLQlFag")
+llm.temperature=0
+prompts=['hi how are you?']
+llm_result=llm._generate(prompts)
+print(llm_result.generations[0][0].text)
