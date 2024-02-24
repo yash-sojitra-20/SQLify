655835c763212784e1aebf2ed9e7bfd58678e3a8
yash-sojitra-20 <yash.sojitra.2020@gmail.com>
2024-02-24 23:45:10 +0530

    Bound the model with some constrain

--- main.py
+++ main.py
@@ -138,3 +138,30 @@ example_selector.select_examples({"Question": "How many Adidas T shirts I have l
 #   'Question': 'If we have to sell all the Levi’s T-shirts today. How much revenue our store will generate without discount?',
 #   'SQLQuery': "SELECT SUM(price * stock_quantity) FROM t_shirts WHERE brand = 'Levi'",
 #   'SQLResult': 'Result of the SQL query'}]
+
+
+#Bound the model with some constrain
+from langchain.chains.sql_database.prompt import PROMPT_SUFFIX, _mysql_prompt
+
+#Generated prompts
+# 1
+# print(_mysql_prompt)
+# You are a MySQL expert. Given an input question, first create a syntactically correct MySQL query to run, then look at the results of the query and return the answer to the input question.
+# Unless the user specifies in the question a specific number of examples to obtain, query for at most {top_k} results using the LIMIT clause as per MySQL. You can order the results to return the most informative data in the database.
+# Never query for all columns from a table. You must query only the columns that are needed to answer the question. Wrap each column name in backticks (`) to denote them as delimited identifiers.
+# Pay attention to use only the column names you can see in the tables below. Be careful to not query for columns that do not exist. Also, pay attention to which column is in which table.
+# Pay attention to use CURDATE() function to get the current date, if the question involves "today".
+
+# Use the following format:
+
+# Question: Question here
+# SQLQuery: SQL Query to run
+# SQLResult: Result of the SQLQuery
+# Answer: Final answer here
+
+# 2
+# print(PROMPT_SUFFIX)
+# Only use the following tables:
+# {table_info}
+
+# Question: {input}
