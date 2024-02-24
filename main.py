fce472b3413d367d665f256e79ffac8db5058b07
yash-sojitra-20 <yash.sojitra.2020@gmail.com>
2024-02-24 23:16:13 +0530

    Embedding using HuggingFaceEmbeddings

--- main.py
+++ main.py
@@ -9,16 +9,17 @@ llm = GoogleGenerativeAI(model="models/text-bison-001", google_api_key=api_key)
 
 
 #Database connection 
-from langchain.utilities import SQLDatabase
+# from langchain.utilities import SQLDatabase
+from langchain_community.utilities import SQLDatabase
 username = "postgres" 
-password = "postgres" 
+password = "yps20" 
 host = "localhost" 
 port = "5432"
-mydatabase = "atliq_tshirts"
+mydatabase = "tshirts"
 
 pg_uri = f"postgresql+psycopg2://{username}:{password}@{host}:{port}/{mydatabase}"
 db = SQLDatabase.from_uri(pg_uri)
-print(db.table_info)
+# print(db.table_info)
 
 
 #sample_query solution using chain
@@ -26,17 +27,19 @@ from langchain.chains import create_sql_query_chain
 db_chain = create_sql_query_chain(llm, db)
 
 # correct -1
-print("qns 1")
+# print("qns 1")
 qns1 = db_chain.invoke({"question": "How many t-shirts do we have left for nike in extra small size and white color?"})
-print(qns1)
+# print(qns1)
 ans1=db.run(qns1)
-print(ans1)
+# print(ans1)
 
 # correct-2
 qns2 = "SELECT SUM(price*stock_quantity) FROM t_shirts WHERE size = 'S'"
 ans2=db.run(qns2)
-print(ans2)
+# print(ans2)
 
+
+#Extra_sample_query solution using chain
 # incorrect-3
 # print("qns 3")
 # qns3 = db_chain.invoke({"question": "If we have to sell all the Levi’s T-shirts today with discounts applied. How much revenue our store will generate (post discounts)?"})
@@ -50,13 +53,14 @@ select sum(a.total_amount * ((100-COALESCE(discounts.pct_discount,0))/100)) as t
 group by t_shirt_id) a left join discounts on a.t_shirt_id = discounts.t_shirt_id
  """
 ans3=db.run(qns3)
-print(ans3)
+# print(ans3)
 
+#correct-4
 qns4 = "SELECT SUM(price * stock_quantity) FROM t_shirts WHERE brand = 'Levi'"
 ans4=db.run(qns4)
-print(ans4)
+# print(ans4)
 
-#incorret-5 -- forgate to sum val...
+#incorret-5 --> forgate to sum val...
 # print("qns 5")
 # qns5 = db_chain.invoke({"question": "How many white color Levi's t shirts we have available?"})
 # print(qns5)
@@ -65,18 +69,20 @@ print(ans4)
 # correct-5
 qns5 = "SELECT sum(stock_quantity) FROM t_shirts WHERE brand = 'Levi' AND color = 'White'"
 ans5=db.run(qns5)
-print(ans5)
+# print(ans5)
 
+
+#Few_shots creation
 few_shots = [
     {'Question' : "How many t-shirts do we have left for Nike in XS size and white color?",
      'SQLQuery' : "SELECT sum(stock_quantity) FROM t_shirts WHERE brand = 'Nike' AND color = 'White' AND size = 'XS'",
      'SQLResult': "Result of the SQL query",
-     'Answer' : qns1},
+     'Answer' : ans1},
     
     {'Question': "How much is the total price of the inventory for all S-size t-shirts?",
      'SQLQuery':"SELECT SUM(price*stock_quantity) FROM t_shirts WHERE size = 'S'",
      'SQLResult': "Result of the SQL query",
-     'Answer': qns2},
+     'Answer': ans2},
     
     {'Question': "If we have to sell all the Levi’s T-shirts today with discounts applied. How much revenue  our store will generate (post discounts)?" ,
      'SQLQuery' : """SELECT sum(a.total_amount * ((100-COALESCE(discounts.pct_discount,0))/100)) as total_revenue from
@@ -84,15 +90,22 @@ few_shots = [
 group by t_shirt_id) a left join discounts on a.t_shirt_id = discounts.t_shirt_id
  """,
      'SQLResult': "Result of the SQL query",
-     'Answer': qns3} ,
+     'Answer': ans3} ,
     
      {'Question' : "If we have to sell all the Levi’s T-shirts today. How much revenue our store will generate without discount?" ,
       'SQLQuery': "SELECT SUM(price * stock_quantity) FROM t_shirts WHERE brand = 'Levi'",
       'SQLResult': "Result of the SQL query",
-      'Answer' : qns4},
+      'Answer' : ans4},
     
     {'Question': "How many white color Levi's shirt I have?",
      'SQLQuery' : "SELECT sum(stock_quantity) FROM t_shirts WHERE brand = 'Levi' AND color = 'White'",
      'SQLResult': "Result of the SQL query",
-     'Answer' : qns5}
+     'Answer' : ans5}
 ]
+
+
+#Embedding using HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
+embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
+# e = embeddings.embed_query("How many white color Levi's shirt I have?")
+# e[:5]
