0f8ea5807636f5ba749b50e74240d82e89e937cd
YASH SOJITRA <128734043+yash-sojitra-20@users.noreply.github.com>
2024-02-25 17:36:26 +0530

    updated : import few_shots from FewShots.py

--- SQLify_Backend.py
+++ SQLify_Backend.py
@@ -10,42 +10,7 @@ from langchain.prompts import FewShotPromptTemplate
 from langchain.prompts.prompt import PromptTemplate
 from dotenv import load_dotenv
 from db_specifications import dbusername,dbpass,dbhost,dbport,databasename
-
-few_shots = [
-    {'Question' : "How many t-shirts do we have left for Nike in XS size and white color?",
-     'SQLQuery' : "SELECT sum(stock_quantity) FROM t_shirts WHERE brand = 'Nike' AND color = 'White' AND size = 'XS'",
-     'SQLResult': "Result of the SQL query",
-     'Answer' : ''},
-    
-    {'Question': "How much is the total price of the inventory for all S-size t-shirts?",
-     'SQLQuery':"SELECT SUM(price*stock_quantity) FROM t_shirts WHERE size = 'S'",
-     'SQLResult': "Result of the SQL query",
-     'Answer': '[(2000,)]'},
-    
-    {'Question': "If we have to sell all the Levi’s T-shirts today with discounts applied. How much revenue  our store will generate (post discounts)?" ,
-     'SQLQuery' : """SELECT sum(a.total_amount * ((100-COALESCE(discounts.pct_discount,0))/100)) as total_revenue from
-(select sum(price*stock_quantity) as total_amount, t_shirt_id from t_shirts where brand = 'Levi'
-group by t_shirt_id) a left join discounts on a.t_shirt_id = discounts.t_shirt_id
- """,
-     'SQLResult': "Result of the SQL query",
-     'Answer': "[(Decimal('2960.00000000000000000000'),)]"
-} ,
-    
-     {'Question' : "If we have to sell all the Levi’s T-shirts today. How much revenue our store will generate without discount?" ,
-      'SQLQuery': "SELECT SUM(price * stock_quantity) FROM t_shirts WHERE brand = 'Levi'",
-      'SQLResult': "Result of the SQL query",
-      'Answer' : '[(4200,)]'},
-    
-    {'Question': "How many white color Levi's shirt I have?",
-     'SQLQuery' : "SELECT sum(stock_quantity) FROM t_shirts WHERE brand = 'Levi' AND color = 'White'",
-     'SQLResult': "Result of the SQL query",
-     'Answer' : '[(None,)]'},
-    
-    {'Question': "show price of blue tshirt of levis with after applying discount.",
-     'SQLQuery' : "SELECT price - (price * pct_discount / 100) AS discounted_price FROM t_shirts JOIN discounts ON t_shirts.t_shirt_id = discounts.t_shirt_id WHERE brand = 'Levi' AND color = 'Blue'",
-     'SQLResult': "Result of the SQL query",
-     'Answer' : '[(21.2500000000000000,),(18.0000000000000000,),(22.0000000000000000,)]'}
-]
+from FewShots import few_shots
 
 username = dbusername
 password = dbpass
